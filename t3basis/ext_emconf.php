d52880795b42cc96b7ca4f316241d581f800f295
Kevin Schüler <78984160+Thyriion@users.noreply.github.com>
2023-01-08 21:45:15 +0100

    Add files via upload

---
+++ t3basis/ext_emconf.php
@@ -0,0 +1,18 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'T3 Basis',
+    'description' => 'Erste Typo3 Template',
+    'category' => 'template',
+    'author' => 'Kevin Schüler',
+    'author_email' => 'kevinschueler2000@gmail.com',
+    'author_company' => '',
+    'state' => 'stable',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.0.0-12.4.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
